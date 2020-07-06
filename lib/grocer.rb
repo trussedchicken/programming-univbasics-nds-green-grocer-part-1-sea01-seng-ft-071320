590be6e8ee9a137621d3e7c2898ff57875783b52
Jeremy Winn <jeremyjwinn@gmail.com>
2020-07-06 17:31:11 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,7 +3,8 @@ def find_item_by_name_in_collection(name, collection)
   counter = 0 
   while counter < collection.length 
     if collection[counter][:item] == name
-      return collection
+      return collection[counter]
+    end
   counter += 1 
   end
 end
