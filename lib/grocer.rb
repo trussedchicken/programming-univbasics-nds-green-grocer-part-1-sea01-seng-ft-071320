82a736c316a3c23f169a74c5ad632e34e7cb317d
Jeremy Winn <jeremyjwinn@gmail.com>
2020-07-06 17:30:57 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,8 +2,10 @@ require 'pry'
 def find_item_by_name_in_collection(name, collection)
   counter = 0 
   while counter < collection.length 
+    if collection[counter][:item] == name
+      return collection
   counter += 1 
-end
+  end
 end
 
 def consolidate_cart(cart)
